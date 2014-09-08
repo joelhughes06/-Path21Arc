7e942c573c9b2ae64742130b0f0e499300421425
Joel Hughes <joelhughes06@netscape.net>
2014-09-07 22:03:19 -0600

    0907142203

--- app/assets/javascripts/lightbox.js
+++ app/assets/javascripts/lightbox.js
@@ -53,11 +53,11 @@
         return false;
       });
     };
-// I deleted out the whole section below
+// I commented out the section below because it was causing duplicate lb-image lb-prev and lb-nav icons to show
     // Build html for the lightbox and the overlay.
     // Attach event handlers to the new DOM elements. click click click
-//    Lightbox.prototype.build = function() {
-//      var self = this;
+    Lightbox.prototype.build = function() {
+      var self = this;
 //      $("<div id='lightboxOverlay' class='lightboxOverlay'></div><div id='lightbox' class='lightbox'><div class='lb-outerContainer'><div class='lb-container'><img class='lb-image' src='' /><div class='lb-nav'><a class='lb-prev' href='' ></a><a class='lb-next' href='' ></a></div><div class='lb-loader'><a class='lb-cancel'></a></div></div></div><div class='lb-dataContainer'><div class='lb-data'><div class='lb-details'><span class='lb-caption'></span><span class='lb-number'></span></div><div class='lb-closeContainer'><a class='lb-close'></a></div></div></div></div>").appendTo($('body'));
       
       // Cache jQuery objects
